420c6354a2883ce80707ac648721190c6e4ba464
Malak88 <malak.bendalla22@gmail.com>
2024-01-10 21:26:40 +0100

    initializing springboot project

---
+++ src/main/java/com/malak/refreshdemo/RefreshdemoApplication.java
@@ -0,0 +1,13 @@
+package com.malak.refreshdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malak/refreshdemo/RefreshdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malak.refreshdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
